f969636ee6d9464b1a8c83e055036214c97f50cf
Gustavo Ribeiro <gustaano881@gmail.com>
2021-06-11 18:56:01 -0300

    Tudo pronto

---
+++ script.js
@@ -0,0 +1,6 @@
+var carregar = setInterval(function(){
+    clearInterval(carregar);
+
+    document.getElementById("imagem-inicial").style.display = "none";
+    document.getElementById("tela-oculta").style.display = "inline";
+},3500);
