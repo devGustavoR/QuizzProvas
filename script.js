03cf3379cdbdc6bd223836b73aa32514723b3625
devgustavor - Home <devgustavor@gmail.com - Home>
2024-05-07 00:20:07 -0300

    Muda tudo

--- script.js
+++
@@ -1,6 +0,0 @@
-var carregar = setInterval(function(){
-    clearInterval(carregar);
-
-    document.getElementById("imagem-inicial").style.display = "none";
-    document.getElementById("tela-oculta").style.display = "inline";
-},3500);
